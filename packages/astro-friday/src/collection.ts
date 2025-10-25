b0b51b8be0245ac2a604254cf0cd0735f25f7268
byronogis <byronogis@outlook.com>
2025-10-25 11:36:31 +0800

    chore: remove unneed 'as' identifiers

--- packages/astro-friday/src/collection.ts
+++ packages/astro-friday/src/collection.ts
@@ -19,21 +19,21 @@ export function getSchema(resolvedConfig: ResolvedConfig) {
   const fKeys = resolvedConfig.post.frontmatterKeys
 
   return z.object({
-    [fKeys.title as 'title']: z.string(),
-    [fKeys.subtitle as 'subtitle']: z.string().optional(),
-    [fKeys.description as 'description']: z.string().optional(),
-    [fKeys.created as 'created']: z.date().default(new Date('1970-01-01')).transform(val => dayjs(val).format('YYYY-MM-DDTHH:mm:ssZ')),
-    [fKeys.modified as 'modified']: z.date().optional().transform(val => val ? dayjs(val).format('YYYY-MM-DDTHH:mm:ssZ') : undefined),
-    [fKeys.author as 'author']: z.string().default(resolvedConfig.author.name), // .transform(val => `${val}${resolvedConfig.author.email ? ` <${resolvedConfig.author.email}>` : ''}`),
-    [fKeys.series as 'series']: z.union([z.string(), z.array(z.string())]).optional().default([]).transform(val => Array.isArray(val) ? val : [val]),
-    [fKeys.tags as 'tags']: z.union([z.string(), z.array(z.string())]).optional().default([]).transform(val => Array.isArray(val) ? val : [val]),
-    [fKeys.keywords as 'keywords']: z.union([z.string(), z.array(z.string())]).optional().default([]).transform(val => Array.isArray(val) ? val : [val]),
-    [fKeys.draft as 'draft']: z.boolean().default(false),
-    [fKeys.lang as 'lang']: z.string().optional().default('en'),
+    [fKeys.title]: z.string(),
+    [fKeys.subtitle]: z.string().optional(),
+    [fKeys.description]: z.string().optional(),
+    [fKeys.created]: z.date().default(new Date('1970-01-01')).transform(val => dayjs(val).format('YYYY-MM-DDTHH:mm:ssZ')),
+    [fKeys.modified]: z.date().optional().transform(val => val ? dayjs(val).format('YYYY-MM-DDTHH:mm:ssZ') : undefined),
+    [fKeys.author]: z.string().default(resolvedConfig.author.name), // .transform(val => `${val}${resolvedConfig.author.email ? ` <${resolvedConfig.author.email}>` : ''}`),
+    [fKeys.series]: z.union([z.string(), z.array(z.string())]).optional().default([]).transform(val => Array.isArray(val) ? val : [val]),
+    [fKeys.tags]: z.union([z.string(), z.array(z.string())]).optional().default([]).transform(val => Array.isArray(val) ? val : [val]),
+    [fKeys.keywords]: z.union([z.string(), z.array(z.string())]).optional().default([]).transform(val => Array.isArray(val) ? val : [val]),
+    [fKeys.draft]: z.boolean().default(false),
+    [fKeys.lang]: z.string().optional().default('en'),
     /**
      * Table of contents (TOC) generation for posts
      */
-    [fKeys.toc as 'toc']: z.object({
+    [fKeys.toc]: z.object({
       /**
        * Enable or disable the table of contents (TOC) generation for posts.
        */
